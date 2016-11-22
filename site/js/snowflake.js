113592e8ed21be02c441176657903e3f7105ba10
itggot-lukas-lonnbro <lukas.lonnbro@itggot.se>
2016-11-22 15:43:34 +0100

    ??

--- site/js/snowflake.js
+++ site/js/snowflake.js
@@ -8,9 +8,9 @@ window.open("autumn_effect.htm","","width=350,height=500")
 }
 
   //Configure below to change URL path to the snow image
-  var snowsrc="./img/snow3.gif"
+  var snowsrc="./img/let_it_rickastley.png"
   // Configure below to change number of snow to render
-  var no = 20;
+  var no = 50;
   // Configure whether snow should disappear after x seconds (0=never):
   var hidesnowtime = 0;
   // Configure how much snow should drop down before fading ("windowheight" or "pageheight")
